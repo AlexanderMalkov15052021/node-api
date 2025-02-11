4e37a274c08a03303b0dccb70da1f10126ae5ccb
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-12 00:32:27 +0700

    Add vercel.json

--- db.js
+++ db.js
@@ -3,11 +3,18 @@ require('dotenv').config()
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+// const pool = new Pool({
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     database: process.env.POSTGRES_DB
+// })
 
 module.exports = pool
